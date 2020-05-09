26cc62b33f6579c8b3fce6ec5644513d51abd84c
clara-9 <38263660+clara-9@users.noreply.github.com>
2020-05-09 17:05:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ Created on Sat May  9 16:14:34 2020
 
 @author: crull
 """
-
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -14,4 +14,5 @@ def hello_name():
     return render_template("main.html")
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
