16aa929db62438b8b7767488975836863b877a82
clara-9 <38263660+clara-9@users.noreply.github.com>
2020-05-09 16:40:41 +0200

    added template

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ Created on Sat May  9 16:14:34 2020
 @author: crull
 """
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_name():
-    return "hello"
+    return render_template("main.html")
 
 if __name__ == '__main__':
     app.run()
